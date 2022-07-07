e59fcd00f49b3db3d702d6c3bcbe888cf515ca0b
aseel-hamed <aseelhamed@gmail.com>
2022-07-07 12:16:59 +0000

    new

--- birthday/models/.ipynb_checkpoints/models-checkpoint.py
+++ birthday/models/.ipynb_checkpoints/models-checkpoint.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class Partner(models.Model):
       _inherit = 'res.partner'
 
-      birthday = fields.Datetime('Date of birth aseel hamed')
+      birthday = fields.Datetime('birth aseel')
