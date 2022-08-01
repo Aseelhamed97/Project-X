093c7bd7c115d63273705447f43bc8eb70a894bc
aseel-hamed <aseelhamed@gmail.com>
2022-08-01 09:39:54 +0000

    test inc=voice

--- birthday/models/models.py
+++ birthday/models/models.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 
-class Partner(models.Model):
-      _inherit = 'sale.order'
+class sale_extend(models.Model):
+  _inherit = 'sale.order'
 
-      birthday = fields.Datetime('Date of birth')
+  reference_number = fields.Char('Reference Number')
+  insurance_number = fields.Char('Insurance Number')
+ 
+  @api.multi
+  def _prepare_invoice(self):
+        res = super(sale_extend, self).prepare_invoice()
+        res['reference_number'] = self.reference_number
+        res['insurance_number'] = self.insurance_number
+        return res
+
+class invoice_extend(models.Model):
+  _inherit = 'account.invoice'
+
+  reference_number = fields.Char('Reference Number')
+  insurance_number = fields.Char('Insurance Number')    
+    
